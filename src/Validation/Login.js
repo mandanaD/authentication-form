6152dc2392c98fd83c256fe118b1077722d9b8b6
Mandana Dashti <dmandana59@gmail.com>
2024-05-20 10:36:54 +0330

    update project

--- src/Validation/Login.js
+++ src/Validation/Login.js
@@ -2,5 +2,5 @@ import * as Yup from "yup"
 
 export const LoginSchema = Yup.object().shape({
     email: Yup.string().email("Email Address Is Not Valid.").required("Email Field Cannot Be Left Blank."),
-    password: Yup.string().required("Email Field Cannot Be Left Blank.")
+    password: Yup.string().required("Password Field Cannot Be Left Blank.")
 })
